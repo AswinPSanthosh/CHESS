5b1b7644219f2f5b5c4e42ca75cb2c94c1bd953b
AswinPSanthosh <aswinps188@gmail.com>
2024-11-26 13:56:44 +0530

    peice initialstate

--- lib/components/Gameboard.dart
+++ lib/components/Gameboard.dart
@@ -14,7 +14,65 @@ class Board extends StatefulWidget {
 }
 
 class _BoardState extends State<Board> {
-   Chesspiece mypawn = Chesspiece(type: ChessPieceType.pawn, iswhite: true, imagepathw: 'assets/pieces/pawnw.png', imagepathb: 'assets/pieces/pawnb.png');
+
+
+
+  void initState() {
+    super.initState();
+    _initialiseBoard();
+    
+  }
+  void _initialiseBoard(){
+List<List<Chesspiece?>>newboard = List.generate(8, (index)=>List.generate(8, (index)=> null));
+
+
+
+//pawn
+for (int i =0 ; i<8; i++)
+{
+  newboard[1][i]=Chesspiece(type: ChessPieceType.pawn, iswhite: false,  imagepathw: 'assets/pieces/pawnw.png', imagepathb: 'assets/pieces/pawnb.png');
+  newboard[6][i]=Chesspiece(type: ChessPieceType.pawn, iswhite: true,  imagepathw: 'assets/pieces/pawnw.png', imagepathb: 'assets/pieces/pawnb.png');
+
+}
+//rook
+ newboard[0][0]=Chesspiece(type: ChessPieceType.rook, iswhite: false,  imagepathw: 'assets/pieces/rookw.png', imagepathb: 'assets/pieces/rookb.png');
+  newboard[0][7]=Chesspiece(type: ChessPieceType.rook, iswhite: false,  imagepathw: 'assets/pieces/rookw.png', imagepathb: 'assets/pieces/rookb.png'); 
+  newboard[7][7]=Chesspiece(type: ChessPieceType.rook, iswhite: true,  imagepathw: 'assets/pieces/rookw.png', imagepathb: 'assets/pieces/rookb.png');
+   newboard[7][0]=Chesspiece(type: ChessPieceType.rook, iswhite: true,  imagepathw: 'assets/pieces/rookw.png', imagepathb: 'assets/pieces/rookb.png');
+
+//knight
+ newboard[0][1]=Chesspiece(type: ChessPieceType.knight, iswhite: false,  imagepathw: 'assets/pieces/knightw.png', imagepathb: 'assets/pieces/knightb.png');
+  newboard[0][6]=Chesspiece(type: ChessPieceType.knight, iswhite: false,  imagepathw: 'assets/pieces/knightw.png', imagepathb: 'assets/pieces/knightb.png'); 
+  newboard[7][6]=Chesspiece(type: ChessPieceType.knight, iswhite: true,  imagepathw: 'assets/pieces/knightw.png', imagepathb: 'assets/pieces/knightb.png');
+   newboard[7][1]=Chesspiece(type: ChessPieceType.knight, iswhite: true,  imagepathw: 'assets/pieces/knightw.png', imagepathb: 'assets/pieces/knightb.png');
+
+//bishop
+ newboard[0][2]=Chesspiece(type: ChessPieceType.bishop, iswhite: false,  imagepathw: 'assets/pieces/bishopw.png', imagepathb: 'assets/pieces/bishopb.png');
+  newboard[0][5]=Chesspiece(type: ChessPieceType.bishop, iswhite: false,  imagepathw: 'assets/pieces/bishopw.png', imagepathb: 'assets/pieces/bishopb.png'); 
+  newboard[7][5]=Chesspiece(type: ChessPieceType.bishop, iswhite: true,  imagepathw: 'assets/pieces/bishopw.png', imagepathb: 'assets/pieces/bishopb.png');
+   newboard[7][2]=Chesspiece(type: ChessPieceType.bishop, iswhite: true,  imagepathw: 'assets/pieces/bishopw.png', imagepathb: 'assets/pieces/bishopb.png');
+
+//queen
+newboard[0][3]=Chesspiece(type: ChessPieceType.queen, iswhite: false,  imagepathw: 'assets/pieces/queenw.png', imagepathb: 'assets/pieces/queenb.png'); 
+  newboard[7][4]=Chesspiece(type: ChessPieceType.queen, iswhite: true,  imagepathw: 'assets/pieces/queenw.png', imagepathb: 'assets/pieces/queenb.png');
+
+
+//king
+newboard[0][4]=Chesspiece(type: ChessPieceType.king, iswhite: false,  imagepathw: 'assets/pieces/kingw.png', imagepathb: 'assets/pieces/kingb.png'); 
+  newboard[7][3]=Chesspiece(type: ChessPieceType.king, iswhite: true,  imagepathw: 'assets/pieces/kingw.png', imagepathb: 'assets/pieces/kingb.png');
+
+
+board = newboard;
+
+}
+
+
+
+
+
+late List<List<Chesspiece?>>board;
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +82,7 @@ class _BoardState extends State<Board> {
             width: 368,
           height: 368,
             decoration: BoxDecoration(border: Border.all(
-              color: board,
+              color: boardcolor,
               width: 10
             )),
             child: GridView.builder(
@@ -37,10 +95,14 @@ class _BoardState extends State<Board> {
               itemCount: 8*8,
               physics: NeverScrollableScrollPhysics(),
               itemBuilder: (BuildContext context, int index) {
+
+
+                int row = index ~/8;
+                int col = index%8;
                 return Padding(
                   padding: const EdgeInsets.all(1.0),
                   child: Square(iswhite: isWhite(index),
-                  piece: mypawn,),
+                  piece: board[row][col],),
                 );
               },
             ),
--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 Color squireWhite = Color(0xFFDEC8B5);
 Color squireBlack =Color(0xFFBB8F67);
-Color board =Color.fromRGBO(131, 92, 58, 1);
+Color boardcolor =Color.fromRGBO(131, 92, 58, 1);
